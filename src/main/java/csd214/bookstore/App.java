d55c262f61fc52a26b5417117413df6d74860075
Rafael Soarescardoso <24017217@saultcollege.ca>
2026-01-19 11:50:31 -0500

    Menu adjustments

--- src/main/java/csd214/bookstore/App.java
+++ src/main/java/csd214/bookstore/App.java
@@ -71,10 +71,10 @@ public class App {
             System.out.println("2. Add Magazine");
             System.out.println("3. Add DiscMag");
             System.out.println("4. Add Ticket");
-            System.out.println("4. Add Notebook");
-            System.out.println("4. Add Pen");
-            System.out.println("4. Add Mouse");
-            System.out.println("4. Add Keyboard");
+            System.out.println("5. Add Notebook");
+            System.out.println("6. Add Pen");
+            System.out.println("7. Add Mouse");
+            System.out.println("8. Add Keyboard");
             System.out.println("99. Exit");
 
             try {
@@ -96,6 +96,7 @@ public class App {
                 case 5: item = new Notebook(); break;
                 case 6: item = new Pen(); break;
                 case 7: item = new Mouse(); break;
+                case 8: item = new Keyboard(); break;
                 default: System.out.println("Invalid selection."); continue;
             }
 
@@ -121,6 +122,10 @@ public class App {
             System.out.println("3. Magazines");
             System.out.println("4. DiscMags");
             System.out.println("5. Tickets");
+            System.out.println("6. Add Notebook");
+            System.out.println("7. Add Pen");
+            System.out.println("8. Add Mouse");
+            System.out.println("9. Add Keyboard");
             System.out.println("99. Exit");
 
             try {
@@ -140,6 +145,10 @@ public class App {
                 case 3: filter = Magazine.class; break;
                 case 4: filter = DiscMag.class; break;
                 case 5: filter = Ticket.class; break;
+                case 6: filter = Notebook.class; break;
+                case 7: filter = Pen.class; break;
+                case 8: filter = Mouse.class; break;
+                case 9: filter = Keyboard.class; break;
                 default: System.out.println("Invalid selection."); continue;
             }
 
@@ -277,6 +286,7 @@ public class App {
             t.description = "Concert: " + faker.rockBand().name();
             t.price = faker.number().randomDouble(2, 50, 150);
             addItem(t);
+
         }
     }
 }
